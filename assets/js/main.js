aa497fdd49a8d3596e1f567c5cc1ec40ac2d493b
hannagabyy <hannarocha000@gmail.com>
2025-05-13 20:39:14 -0300

    Finalizando parte base dos projetos

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+// Mudar fundo header
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    
+    this.scrollY >= 50 ? header.classList.add('scroll-header') 
+                       : header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
